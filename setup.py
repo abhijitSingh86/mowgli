8077d5222b76c9c008d0a545a87b14fd7e64e372
Felistas Ngumi <felistas@Felistass-MacBook-Pro.local>
2020-03-03 13:19:41 +0100

    add setup tools

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mowgli",
+    version="0.0.1",
+    author="Balu Chatbot",
+    author_email="balu-chatbot@thoughtworks.com",
+    description="Intent classifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abhijitSingh86/mowgli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
