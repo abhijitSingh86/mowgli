83fc4fcd92623cb840ecabca4aa3254dfbf19b3a
abhijit Singh <abhijitsingh309@gmail.com>
2020-03-06 14:28:22 +0100

    Implement Network

--- mowgli/model/datasets.py
+++ mowgli/model/datasets.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from tensorflow import keras
 from tensorflow.keras import layers
 
+
 def parse(line):
     split_line = tf.strings.split(line, sep=',', maxsplit=1)
     return tf.strings.to_number(split_line[0], out_type=tf.dtypes.int32), split_line[1]
@@ -24,19 +25,19 @@ def encode_vectorize(dataset, vocabulary_count):
     return encoded_matrix, vectorizer
 
 
-def build_network(bags, vectorizer, labels):
-    print("**********",bags)
+def split(data):
+    length = int(len(data)*.8)
+    return data[0:length], data[length:]
 
+def build_network(bags, vectorizer, labels):
     bags = bags.toarray()
-    input = keras.Input(shape=[len(bags[0]),], name='tokens')
+    input = keras.Input(shape=[len(bags[0]), ], name='tokens')
     print(input)
 
     x = layers.Dense(8)(input)
     x = layers.Dense(8, activation="softmax")(x)
     outputs = layers.Dense(len(labels), name='IntentClassification')(x)
     print(outputs)
-    '''
-
     model = keras.Model(inputs=input, outputs=outputs)
     model.compile(optimizer=keras.optimizers.RMSprop(),  # Optimizer
                   # Loss function to minimize
@@ -44,15 +45,16 @@ def build_network(bags, vectorizer, labels):
                   # List of metrics to monitor
                   metrics=['sparse_categorical_accuracy'])
     print('# Fit model on training data')
-    history = model.fit(bags[:(int(len(bags)*.8))], labels[:(int(len(labels)*.8))],
-                        batch_size=64,
+
+    train_x,validate_x = split(bags)
+    train_y,validate_y = split(labels)
+    history = model.fit(train_x, train_y,
+                        batch_size=3,
                         epochs=3,
                         # We pass some validation for
                         # monitoring validation loss and metrics
                         # at the end of each epoch
-                        validation_data=(bags[:(int(len(bags)*.2))], labels[:(int(len(labels)*.2))]))
+                        validation_data=(validate_x,validate_y))
 
     print('\nhistory dict:', history.history)
-    '''
-
-    #return model
+    return model
